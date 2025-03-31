f83d056860c0c41939ba47592a8c0aad85bfe125
Devansh <devanshkg19@gmail.com>
2025-03-31 14:41:10 +0530

    Commit

---
+++ routes/user.routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+const userRouter=Router();
+
+userRouter.get('/', (req, res) =>
+    res.send({ title:'GET all users'})
+)
+userRouter.get('/:id', (req, res) =>
+    res.send({ title:'GET user details'})
+)
+userRouter.post('/', (req, res) =>
+    res.send({ title:'Create new User'})
+)
+userRouter.put('/:id', (req, res) =>
+    res.send({ title:'Update user'})
+)
+userRouter.delete('/:id', (req, res) =>
+    res.send({ title:'Delete user'})
+)
+export default userRouter;
